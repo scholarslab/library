d9e313f1f519a5031bb27cee6ee32ade84e7880a
Amanda Visconti <amandavisconti@users.noreply.github.com>
2021-01-05 15:15:50 -0500

    Updating to use Slack instead of Google auth
    Via user fork explained here: https://github.com/nytimes/library/issues/81
    Currently, Library lets anyone with the email domain you specify access the site; testing to see whether this can help restrict access to SLab staff.

--- server/userAuth.js
+++ server/userAuth.js
@@ -3,7 +3,7 @@
 const passport = require('passport')
 const session = require('express-session')
 const md5 = require('md5')
-const GoogleStrategy = require('passport-google-oauth20')
+const SlackStrategy = require('passport-slack-oauth2').Strategy;
 
 const log = require('./logger')
 const {stringTemplate: template} = require('./utils')
@@ -11,13 +11,18 @@ const {stringTemplate: template} = require('./utils')
 const router = require('express-promise-router')()
 const domains = new Set(process.env.APPROVED_DOMAINS.split(/,\s?/g))
 
-passport.use(new GoogleStrategy.Strategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/redirect',
-  userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
-  passReqToCallback: true
-}, (request, accessToken, refreshToken, profile, done) => done(null, profile)))
+passport.use(new SlackStrategy({
+    clientID: process.env.SLACK_CLIENT_ID,
+    clientSecret: process.env.SLACK_CLIENT_SECRET,
+    skipUserProfile: false,
+    callbackURL: process.env.SLACK_CALLBACK_URL,
+    scope: ['identity.basic', 'identity.email', 'identity.avatar', 'identity.team']
+  },
+  (accessToken, refreshToken, profile, done) => {
+    // optionally persist user data into a database
+    done(null, profile);
+  }
+));
 
 router.use(session({
   secret: process.env.SESSION_SECRET,
@@ -33,22 +38,17 @@ router.use(passport.session())
 passport.serializeUser((user, done) => done(null, user))
 passport.deserializeUser((obj, done) => done(null, obj))
 
-router.get('/login', passport.authenticate('google', {
-  scope: [
-    'https://www.googleapis.com/auth/userinfo.email',
-    'https://www.googleapis.com/auth/userinfo.profile'
-  ],
-  prompt: 'select_account'
-}))
+router.get('/login', passport.authorize('Slack'));
 
 router.get('/logout', (req, res) => {
   req.logout()
   res.redirect('/')
 })
 
-router.get('/auth/redirect', passport.authenticate('google'), (req, res) => {
-  res.redirect(req.session.authRedirect || '/')
-})
+router.get('/auth/redirect',
+  passport.authenticate('Slack', { failureRedirect: '/login' }),
+  (req, res) => res.redirect(req.session.authRedirect || '/')
+);
 
 router.use((req, res, next) => {
   const isDev = process.env.NODE_ENV === 'development'
@@ -69,15 +69,7 @@ router.use((req, res, next) => {
 })
 
 function isAuthorized(user) {
-  const [{value: userEmail = ''} = {}] = user.emails || []
-  const [userDomain] = userEmail.split('@').slice(-1)
-  const checkRegexEmail = () => {
-    const domainsArray = Array.from(domains)
-    for (const domain of domainsArray) {
-      if (userDomain.match(domain)) return true
-    }
-  }
-  return domains.has(userDomain) || domains.has(userEmail) || checkRegexEmail()
+  return domains.has(user.team.domain);
 }
 
 function setUserInfo(req) {
@@ -90,9 +82,9 @@ function setUserInfo(req) {
     return
   }
   req.userInfo = req.userInfo ? req.userInfo : {
-    email: req.session.passport.user.emails[0].value,
-    userId: req.session.passport.user.id,
-    analyticsUserId: md5(req.session.passport.user.id + 'library')
+    email: req.session.passport.user.displayName,
+    userId: req.session.passport.user.user.id,
+    analyticsUserId: md5(req.session.passport.user.user.id + 'library')
   }
 }
 
